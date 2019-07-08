25fda5590e291dec61a89baab5040a48db3a411a
Merli Mejia <merlimejia25@gmail.com>
2019-07-07 20:49:20 -0400

    Initial commit

---
+++ precache-manifest.6ce9442c8cc07a211ee4f5a2bc9e471e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba4d51bd8913dbaad7db132125b2b47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9c47ffc99e7e83bcaf9",
+    "url": "/static/css/main.2b9d6b8e.chunk.css"
+  },
+  {
+    "revision": "cbefcbf2a2910fa93edc",
+    "url": "/static/js/2.47374caa.chunk.js"
+  },
+  {
+    "revision": "a9c47ffc99e7e83bcaf9",
+    "url": "/static/js/main.ec541c50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1ad3f26ccf8e15f3eaeca67885e85ecb",
+    "url": "/static/media/270.1ad3f26c.svg"
+  },
+  {
+    "revision": "9b85c599e0da36f62992188c4dad16b6",
+    "url": "/static/media/AIR.9b85c599.svg"
+  },
+  {
+    "revision": "3823f6efa65042637cb2ac3450ae81d2",
+    "url": "/static/media/AIRMAX.3823f6ef.svg"
+  },
+  {
+    "revision": "d224a50b07fe3b135395d508fb78778b",
+    "url": "/static/media/MAX.d224a50b.svg"
+  },
+  {
+    "revision": "c74df19d4fabae219411a2862b83d601",
+    "url": "/static/media/nike.c74df19d.svg"
+  },
+  {
+    "revision": "f43467199474501459c20c7a748e5723",
+    "url": "/static/media/nikeL.f4346719.png"
+  }
+]);
